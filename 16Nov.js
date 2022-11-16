0b902d9302c024c92fd9ab4649a33ecf816c8189
Srinu Bali <balisrinu313@gmail.com>
2022-11-16 19:10:37 +0530

    First Commit in our Project

---
+++ 16Nov.js
@@ -0,0 +1,3 @@
+console.log("tet")
+console.log("tet")
+console.log("tet")
---
+++ index.js
@@ -0,0 +1,121 @@
+// var limit = 25;
+// for (var start = 2; start <= limit; start++ ){
+//    var number = start;
+//    var count =0;
+//    for(var i=1; i<= number ; i++) {
+//     var count =0;
+//      if (number % i ==0){
+//          count++
+//      }
+     
+//    }   
+// }
+// if (count ==2){
+//     console.log(number)
+// }
+// console.log("bali");
+
+//we have to check whether the numbe ris prime or not?
+        // var number = 941;
+        // var count = 0;
+        // for(let i =1 ; i<=number ; i++ ){
+        // if(number % i == 0){
+        //     count++; 
+        // }
+        
+        // }
+        // if(count == 2){
+        //     console.log(`${number} is prime number`)
+        // }else{
+        //     console.log(`${number} is not primenumber`)
+        // }
+
+
+//   function prime(number){
+//          let count = 0;
+//         //  let factors = []
+//         for (let x = 1; x <= number; x++ ){
+//             // count = 0; //block-scoped, so throws wrong answer
+//            if(number % x == 0){
+//               count++;
+//              // factors.push(x);
+//            }
+//         }
+//          // console.log(count)
+//           if (count == 2 ){
+//             console.log(`${number} is a prime number`)
+//           }else{
+//             console.log(`${number} is not  a prime number`)
+//           }
+//         //  console.log(factors)
+//        }
+//       prime(100)
+// console.log("Dear javascript , I love You")
+
+
+// ARRAY OF PRIME NUMBERS UPTO THE LIMIT.....
+// var limit = 1000
+// var arrPrime = []
+// for (var start = 2; start <= limit ; start++){  
+//   var number = start; 
+//   var count = 0;
+// //   console.log(number);
+
+//   for(var i=1; i <= number; i++){
+//       if (number % i ==0){
+//           count++;
+//       }
+//   }
+
+//     if(count == 2){
+//         // console.log(number)
+//         arrPrime.push(number)
+        
+//     }
+  
+// }
+// console.log(arrPrime)
+
+// console.log("SRINU");
+// console.log("madhu is an javascript developer");
+
+
+// no, of spaces in a string....
+/*
+var str = "a quick brown fix jumps over a lazy dog";
+var count = 0 ;
+for (var i = 0; i< str.length ; i++ ){
+        if(str[i] == " "){
+          count++;
+        }
+}
+console.log(count);
+*/
+
+//print alll the Dats........
+
+/*
+var year =2022;
+var days = 31;
+
+for(var i = 1 ; i<= 12 ; i++){
+        if (i == 4 || i == 6 || i==9 || i==11){
+                days = 30
+        }
+        if(i == 2){
+             if(year %4 == 0){
+                  days =29
+             }else{
+                   days = 28
+             }
+        }
+
+   for(var j =1; j<=days ; j++){
+       
+      console.log(j +"-" + i + "-" + year );
+   }
+
+}
+*/
+
+//
